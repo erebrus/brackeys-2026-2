aa55e019a422935b6b5df0bf43c6c2ab3cbc9091
Cláudio Coelho <claudio.coelho@solenix.ch>
2026-08-30 10:57:02 +0200

    heart icon

--- godot/src/systems/greenhouse/plant/health_icon.gd
+++ godot/src/systems/greenhouse/plant/health_icon.gd
@@ -1,14 +1,21 @@
-extends PanelContainer
+extends Control
 
 
 @export var plant: Plant
 
-@export var separate_met_and_unmet:= false
+#@export var separate_met_and_unmet:= false
 
 var tween: Tween
 
+@onready var heart: TextureRect = $heart
 
-@onready var change_label: Label = %ChangeLabel
+@onready var plus_1: TextureRect = $plus1
+@onready var plus_2: TextureRect = $plus2
+@onready var plus_3: TextureRect = $plus3
+@onready var minus_1: TextureRect = $minus1
+@onready var minus_2: TextureRect = $minus2
+@onready var minus_3: TextureRect = $minus3
+
 
 
 func _ready() -> void:
@@ -23,19 +30,27 @@ func _on_met_requirements_changed(met: int, unmet:int) -> void:
 	
 	var text = ""
 	
-	if separate_met_and_unmet:
-		text = "".lpad(unmet, "-") + "".lpad(met, "+") 
+	#if separate_met_and_unmet:
+		#text = "".lpad(unmet, "-") + "".lpad(met, "+") 
+	
+	var total = met - unmet
+	
+	if total > 0:
+		text = "".lpad(total, "+")
+	elif total < 0:
+		text = "".lpad(-total, "-")
 	else:
-		var total = met - unmet
-		if total > 0:
-			text = "".lpad(total, "+")
-		elif total < 0:
-			text = "".lpad(-total, "-")
-		else:
-			text = "="
+		text = "="
+
+	minus_3.visible = total <= -3
+	minus_2.visible = total == -2
+	minus_1.visible = total == -1
+	heart.visible = total == 0
+	plus_1.visible = total == 1
+	plus_2.visible = total == 2
+	plus_3.visible = total >= 3
+		
 	
-	
-	change_label.text = text
 	modulate.a = 1.0
 	
 	tween = create_tween()
